ea99711b51d4c92554a4de2796b0925235b93caf
CLE4N-HS <cleen.noe@gmail.com>
2023-12-18 13:02:22 +0100

    add files

---
+++ DuckHunt/DuckHunt/tools.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include "SFML/Graphics.h"
+
+#define WINDOW_LENGTH 1920
+#define WINDOW_HEIGHT 1080
+#define EPSILON (float)1e-6
+
+sfTime sftime;
+sfClock* sfclock;
+
+sfVector2f vector2f(float _x, float _y);
+int iRand(int _min, int _max);
+void initTools();
+void restartClock();
+float GetDeltaTime();
+sfVector2f CreateVector(sfVector2f _pos1, sfVector2f _pos2);
+sfVector2f AddVectors(sfVector2f _v1, sfVector2f _v2);
+sfVector2f SubstractVectors(sfVector2f _v1, sfVector2f _v2);
+float DotProduct(sfVector2f _v1, sfVector2f _v2);
+float GetMagnitude(sfVector2f _vector);
+sfVector2f Normalize(sfVector2f _vector);
+float GetSqrMagnitude(sfVector2f _vector);
+sfVector2f MultiplyVector(sfVector2f _vector, float _multiply);
+sfBool Equals(sfVector2f _v1, sfVector2f _v2);
